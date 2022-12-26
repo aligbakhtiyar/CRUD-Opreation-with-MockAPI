732843616c0c3564a91d0d4f323b45aa4666a4b7
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2022-12-26 10:34:57 +0530

    create component

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,28 +1,37 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Create = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-
-    
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const header = { "Access-Control-Allow_Origin": "*" };
+  const handleSubmit = () => {
+    console.log('clicked')
+    axios.post("https://63a920e1f4962215b58db2d5.mockapi.io/crud-mockapi", {
+      name: name,
+      email: email,
+      header,
+    });
+  };
 
   return (
     <>
       <form>
         <div className="mb-3">
           <label className="form-label">Name</label>
-          <input type="text" className="form-control" 
-          onChange={(e) =>
-            setName(e.target.value)
-          } 
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div className="mb-3">
           <label className="form-label"> Email</label>
-          <input type="email" className="form-control" 
-          onChange={(e) =>
-            setEmail(e.target.value)
-          } 
+          <input
+            type="email"
+            className="form-control"
+            aria-describedby="emailHelp"
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -31,10 +40,14 @@ const Create = () => {
           {email}
         </div>
 
-        <button type="submit" className="btn btn-primary">
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={handleSubmit}
+        >
           Submit
         </button>
-      </form>       
+      </form>
     </>
   );
 };
